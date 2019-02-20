de854f7dac09153a81995e32d2d10409dbe0c624
Daniel Anner <daniel.anner@danstechsupport.com>
2019-02-20 12:09:37 -0500

    Completed - pre commenting

--- Homework Assignments/HW4/solution/solution.java
+++ Homework Assignments/HW4/solution/solution.java
@@ -6,12 +6,12 @@ import csis4463.*;
 public class solution {
 	public static void main(String[] args) {
 		//Hash map or 2d array for storing values of state averages
-		
+		double[][] expanded = new double[6][6];
+		double[][] generated = new double[6][6];
+		double[][] memory = new double[6][6];
 		
 		for(int i = 2; i <= 12; i+=2) { //loop through the required sizes 2,4,6,8,10,12
-			double[][] expanded = new double[6][6];
-			double[][] generated = new double[6][6];
-			double[][] memory = new double[6][6];
+			
 			for (int j = 0; j < 100; j++) {
 				SlidingTilePuzzle puzzle = new SlidingTilePuzzle(3, 3, i);
 
@@ -58,10 +58,36 @@ public class solution {
 				}
 			}
 			
-			expanded[(i/2)-1][0] /= 100;
-			generated[(i/2)-1][0] /= 100;
-			memory[(i/2)-1][0] /= 100;
+			for (int j = 0; j < 6; j++) {
+				expanded[(i/2)-1][j] /= 100;
+				generated[(i/2)-1][j] /= 100;
+				memory[(i/2)-1][j] /= 100;
+			}
 		}
+		
+		System.out.println("Average Number of States Expanded");
+		printRowHeader();
+		for(int i = 2; i <= 12; i+=2) {
+			printColumnHeader(i);
+            printRow(expanded[(i/2)-1]);
+		}
+		System.out.println();
+		
+		System.out.println("Average Number of States Generated");
+		printRowHeader();
+		for(int i = 2; i <= 12; i+=2) {
+			printColumnHeader(i);
+            printRow(generated[(i/2)-1]);
+		}
+		System.out.println();
+		
+		System.out.println("Max States in Memory");
+		printRowHeader();
+		for(int i = 2; i <= 12; i+=2) {
+			printColumnHeader(i);
+            printRow(memory[(i/2)-1]);
+		}
+		System.out.println();
 	}
 	
 	private static double[] ucs(SlidingTilePuzzle thePuzzle) {
@@ -101,4 +127,19 @@ public class solution {
 		
 		return new double[] {solution.getNumberOfStatesExpanded(), solution.getNumGenerated(), solution.getNumberOfStatesInMemory()};
 	}
+	public static void printRow(double[] row) {
+        for (double i : row) {
+            System.out.print(i);
+            System.out.print("\t");
+        }
+        System.out.println();
+    }
+	public static void printRowHeader() {
+		System.out.print("L\tUCS\tA*1\tA*2\tID\tIDA*1\tIDA*2");
+        System.out.println();
+    }
+	public static void printColumnHeader(int len) {
+        System.out.print(len);
+        System.out.print("\t");
+    }
 }
