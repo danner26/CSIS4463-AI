a38fc7c100aecc5683749cd00ad0da1bb5343194
Unknown <samisidoro@gmail.com>
2019-03-27 19:47:12 -0400

    readFile method
    Method reads a file line by line

--- Homework Assignments/HW8/Sudoku.py
+++ Homework Assignments/HW8/Sudoku.py
@@ -1,1 +1,9 @@
-print("Hello World")
+# Reads in the file and prints line by line
+def readFile():
+    file = open("s01a.txt","r")
+    f1 = file.readlines()
+    for x in f1:
+        print(x)
+
+if __name__ == "__main__":
+    readFile()
